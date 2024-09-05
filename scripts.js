08b652aca2a65534031df19dd5a100187b1f5976
Tanisha Deshmukh <tanishadeshmukh38@gmail.com>
2024-09-05 15:54:20 +0530

    First commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
